dbb309e90b563404ca560d87e630fab5e75dbe84
User <user@MacBook-Pro-von-MCM-10703.local>
2023-11-17 16:09:25 +0100

    to modules

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
-const contact = require('./contacts.js');
-const { Command } = require('commander');
+import { Command } from "commander";
+import contact from "./contacts.js";
 const program = new Command();
 program
-    .option('-a, --action <type>', 'choose action')
-    .option('-i, --id <type>', 'user id')
-    .option('-n, --name <type>', 'user name')
-    .option('-e, --email <type>', 'user email')
-    .option('-p, --phone <type>', 'user phone');
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
-    switch (action) {
-        case 'list':
-            (async () => {
-                try {
-                    console.log(await contact.listContacts());
-                } catch (e) {
-                    console.error('error', e)
-                }
-            })()
-            break;
+  switch (action) {
+    case "list":
+      (async () => {
+        try {
+          console.table(await contact.listContacts());
+        } catch (e) {
+          console.error("error", e);
+        }
+      })();
+      break;
 
-        case 'get':
-            // ... id
-            (async () => {
-                try {
-                    console.log(await contact.getContactById(id));
-                } catch (e) {
-                    console.error('error', e)
-                }
-            })()
-            break;
+    case "get":
+      // ... id
+      (async () => {
+        try {
+          console.log(await contact.getContactById(id));
+        } catch (e) {
+          console.error("error", e);
+        }
+      })();
+      break;
 
-        case 'add':
-            // ... name email phone
-            (async () => {
-                try {
-                    console.log(await contact.addContact(name, email, phone));
-                } catch (e) {
-                    console.error('error', e)
-                }
-            })()
-            break;
+    case "add":
+      // ... name email phone
+      (async () => {
+        try {
+          console.log(await contact.addContact(name, email, phone));
+        } catch (e) {
+          console.error("error", e);
+        }
+      })();
+      break;
 
-        case 'remove':
-            // ... id
-            (async () => {
-                try {
-                    console.log(await contact.removeContact(id));
-                } catch (e) {
-                    console.error('error', e)
-                }
-            })()
-            break;
+    case "remove":
+      // ... id
+      (async () => {
+        try {
+          console.log(await contact.removeContact(id));
+        } catch (e) {
+          console.error("error", e);
+        }
+      })();
+      break;
 
-        default:
-            console.warn('\x1B[31m Unknown action type!');
-    }
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 }
 
 invokeAction(argv);
